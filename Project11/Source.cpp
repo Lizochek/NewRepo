bdfab437a64aafc9bc3665ea687ab2c691f9bd6d
Елизавета Куличенко <Elicoolich@yandex.ru>
2020-07-09 11:42:35 +0300

    template

--- Project11/Source.cpp
+++ Project11/Source.cpp
@@ -3,64 +3,110 @@
 
 using namespace std;
 
+template <typename T>
 class Stack {
-	Stack() :_top(NULL), _size(0) {}
+public:
+	Stack() : _top(NULL), _size(0) {}
 
 	struct Node
 	{
-		Node(int el) :data(el) {}
-		Node(int el, Node* node) :data(el), next(node) {}
+		Node(T el) :data(el) {}
+		Node(T el, Node* node) :data(el), next(node) {}
 		Node* next;
-		int data;
+		T data;
 	};
+
+	void push(const T);
+	void pop();
+	T& top();
+	int size();
+	bool empty();
+
 private:
 	Node* _top;
 	int _size;
-
-	void push_back(const int el)
-	{
-
-		if (_top != NULL)
-		{
-			Node* temp = new Node(el, _top);//создаЄм новый нод, инициализируем его переменной el и он указывает на _top(текущий конец стека)
-			_top = temp; //новый элемент делаем последним
-		}
-		else
-		{
-			_top = new Node(el); //создаЄм новый нод и инициализируем его переменной el	
-		}
-		_size++;
-	}
-	void Pop()
-	{
-
-		if (_top == NULL) cout << "Error: Out of range";
-
-		Node* temp = _top; //сохран€ем последний элемент во временную переменную
-		_top = _top->next; //предпоследний(тот,на который указывает последний) элемент делаем последним
-		delete temp; //удал€ем последний элемент
-		_size--;
-	}
-
-	int& top()
-	{
-		if (_top == NULL) cout << "Error: Stack is empty!";
-
-		return _top->data;
-
-	}
-	int size()
-	{
-		return _size;
-	}
-	bool empty()
-	{
-		return _size == 0 ? true : false;
-	}
 };
+template <typename T>
+void Stack<T>::push(const T el)
+{
+
+	if (_top != NULL)
+	{
+		Node* temp = new Node(el, _top);//создаЄм новый нод, инициализируем его переменной el и он указывает на _top(текущий конец стека)
+		_top = temp; //новый элемент делаем последним
+	}
+	else
+	{
+		_top = new Node(el); //создаЄм новый нод и инициализируем его переменной el	
+	}
+	_size++;
+}
+template <typename T>
+void Stack<T>::pop()
+{
+
+	if (_top == NULL) cout << "Error: Out of range";
+
+	Node* temp = _top; //сохран€ем последний элемент во временную переменную
+	_top = _top->next; //предпоследний(тот,на который указывает последний) элемент делаем последним
+	delete temp; //удал€ем последний элемент
+	_size--;
+}
+template <typename T>
+T& Stack<T>::top()
+{
+	if (_top == NULL) cout << "Error: Stack is empty!";
+
+	return _top->data;
+
+}
+template <typename T>
+int Stack<T>::size()
+{
+	return _size;
+}
+template <typename T>
+bool Stack<T>::empty()
+{
+	return _size == 0 ? true : false;
+}
+
+int get(Stack <int> st, int  i);
 int main()
 {
-	int top, size;
-	cin >>
-		Stack st()
+	Stack<int> st;
+	int n, a;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a;
+		st.push(a);
+	}
+	int m;
+	cin >> m;
+	for (int i = 0; i < m; i++)
+	{
+		cin >> a;
+		cout << get(st, a) << endl;
+	}
+
+}
+int get(Stack <int> st, int  i)
+{
+	int s = 0, n;
+	Stack <int> st1;
+	n = st.size() - i - 1;
+
+	for (int j = 0; j < n; j++)
+	{
+		st1.push(st.top());
+		st.pop();
+	}
+	s = st.top();
+	for (int j = 0; j < n; j++)
+	{
+		st.push(st1.top());
+
+	}
+	return s;
 }
