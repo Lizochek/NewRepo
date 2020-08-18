b7da67f708bdc75b5342edccd1e9b3d3961d54db
Елизавета Куличенко <Elicoolich@yandex.ru>
2020-08-18 12:36:05 +0300

    д/з:зная уровни,вывести дерево по уроням

--- Project11/Source.cpp
+++ Project11/Source.cpp
@@ -5,8 +5,8 @@ using namespace std;
 struct Node
 {
 	Node(int el) :data(el) {}
-	Node* right=NULL;
-	Node* left=NULL;
+	Node* right = NULL;
+	Node* left = NULL;
 	int data;
 };
 void add(Node*, int);
@@ -15,38 +15,42 @@ void print(Node*);
 
 int main()
 {
-	Node* root=new Node(64);
+	Node* root = new Node(64);
 
 	root->left = new Node(22);
 	root->left->left = new Node(17);
 	root->left->right = new Node(37);
 	root->left->right->right = new Node(54);
-	root->left->right->left= new Node(23);
+	root->left->right->left = new Node(23);
 	root->left->right->left->right = new Node(35);
 
 	root->right = new Node(112);
 	root->right->right = new Node(114);
-	root->right->left= new Node(72);
+	root->right->left = new Node(72);
 
 	print(root);
 }
 void print(Node* root)
 {
-	queue<Node*> q;
-	q.push(root);
+	queue<pair<Node*,int>* > q;
+
+	q.push(new pair<Node*,int>(root, 0));
 	while (!q.empty())
 	{
 		//извлекаем элемент
 
-		Node* r=NULL;
-		r=q.front();
+		pair<Node*, int>* r = NULL;
+		r = q.front();
 		q.pop();
 		//обрабатываем элемент
-		if(r!=NULL)
-		cout << r->data<<" ";
+		if (r->first == NULL) continue;
+		cout << r->first->data <<"-"<<r->second<< " ";
+
 		//добавляем прямых потомков элемента в очередь
-		q.push(r->right);
-		q.push(r->left);
+		q.push(new pair<Node*,int>(r->first->left,r->second+1));
+		q.push(new pair<Node*, int>(r->first->right, r->second + 1));
+		
+		
 
 	}
 }
