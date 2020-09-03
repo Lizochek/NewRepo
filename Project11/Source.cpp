7186fcd3e6336ed5aab9539c959350c90c9f1ff3
Елизавета Куличенко <Elicoolich@yandex.ru>
2020-09-03 12:52:56 +0300

    проверить строку на палиндром

--- Project11/Source.cpp
+++ Project11/Source.cpp
@@ -1,18 +1,27 @@
 #include<iostream>
 using namespace std;
-void rec2(int n)
+void rec2(string s)
 {
-	if (n==0)
-		return ;
-	rec2(n / 10);
-	cout<< n % 10<<" ";
+	cout << s << endl;
+	if (s.size() <= 1 )
+	{
+		cout << "yes";
+		return;
+	}
+	if (s[0] != s[s.size() - 1])
+	{
+		cout << "no";
+		return;
+	}
+	rec2(s.substr(1, s.size()-2));
+	
 		
 	//sum = n % 10 + (n / 10) % 10 + (n / 10 / 10) % 10 + ... + 0;
 }
 int main()
 {
-	int n;
-	cin >> n;
+	string s;
+	cin >> s;
 	//rec(n);
-	rec2(n);
+	rec2(s);
 }
