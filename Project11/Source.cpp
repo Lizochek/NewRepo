ec0f6577d68efae61b27d6f388e043cb1d8cb4e5
Елизавета Куличенко <Elicoolich@yandex.ru>
2020-09-03 09:13:09 +0300

    ...

--- Project11/Source.cpp
+++ Project11/Source.cpp
@@ -4,14 +4,15 @@
 #include<map>
 using namespace std;
 void dfs(const vector<vector<int> >&, int*, bool*, int);
+void wfs(const vector<vector<int> >&, int*, bool*, int);
 int main()
 {
-	
-	int n,m;
+
+	int n, m;
 	cin >> n >> m;
 	vector <vector<int> > graph(n);
 
-	int *colors = new int [n];
+	int* colors = new int[n];
 	bool* used = new bool[n];
 	for (int i = 0; i < n; i++)
 	{
@@ -20,18 +21,26 @@ int main()
 	}
 	for (int i = 0; i < m; i++)
 	{
-		int from,to;
+		int from, to;
 		cin >> from >> to;
 		graph[--from].push_back(--to);
 	}
 
-	for (int i = 0; i < n; i++) dfs(graph, colors, used, i);
+	for (int i = 0; i < n; i++) wfs(graph, colors, used, i);
+
 	multimap <int, int> sim;
-	for (int i = 0; i < n; i++) 
-		sim.insert(colors[i], i);
+	for (int i = 0; i < n; i++)
+		sim.insert({ colors[i], i });
+	int prev_color = 0;
+	for (const auto& p : sim)
+	{
+
+		if (p.first != prev_color)cout << endl;
+		cout << p.second+1 << " ";
+		prev_color = p.first;
+	}
 
 
-	
 	/*
 	1. Запускать поиск из каждой вершины,пока не посетим все вершины
 	1.1 Завести bool массив посещённости вершин
@@ -44,7 +53,40 @@ int main()
 	*/
 
 }
-void dfs(const vector<vector<int> >& gr, int* clr, bool* u, int node)  
+void dfs(const vector<vector<int> >& gr, int* clr, bool* u, int node)
 {
+	if (u[node])return;
+	u[node] = true;
+	for (auto i : gr[node])
+	{
+		int old_clr = clr[i];
+		for (int j = 0; j < gr.size(); j++) {
+			if (clr[j] == old_clr)
+				clr[j] = clr[node];
+		}
+		dfs(gr, clr, u, i);
+	}
+}
+void wfs(const vector<vector<int> >& gr, int* clr, bool* u, int node)
+{
+	queue<int> numb_versh;
+	numb_versh.push(node);
+	while (!numb_versh.empty()) 
+	{
+		int temp=numb_versh.front();
+		numb_versh.pop();
+		if (u[temp]) continue;
+		u[temp] = true;
+		for (auto i : gr[temp])
+		{
+			int old_clr = clr[i];
+			for (int j = 0; j < gr.size(); j++) {
+				if (clr[j] == old_clr)
+					clr[j] = clr[temp];
+			}
+			numb_versh.push(i);
+		}
+	}
+	
 
 }
