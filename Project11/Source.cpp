9588bc07f6f8710719a17249bb60b084587db1e4
Елизавета Куличенко <Elicoolich@yandex.ru>
2020-09-03 09:55:39 +0300

    wrote a method pop()

--- Project11/Source.cpp
+++ Project11/Source.cpp
@@ -4,7 +4,7 @@ template <typename T>
 class List {
 
 public: struct Node {
-	Node() { this->next = NULL; this->prev = NULL;};
+	Node() { this->next = NULL; this->prev = NULL; };
 	Node(T value, Node* next, Node* prev)
 	{
 		this->next = next;
@@ -22,9 +22,9 @@ public: struct Node {
 	Node* next;
 	Node* prev;
 };
-private: Node* head; 
-		 int _size;
-	   
+private: Node* head;
+	   int _size;
+
 public:
 	List();
 	List(const List&);
@@ -56,7 +56,7 @@ List<T>::List(const List& k)
 	когда вызвается:
 	List <int> a;
 	List<int> b;
-	a = b; 
+	a = b;
 	передаем в фун-цию по значению
 	function(a);
 	возвращаем из функции по значению
@@ -67,29 +67,29 @@ List<T>::List(const List& k)
 	if (k.head == NULL) return;
 
 	head = new Node(k.head->value);
-	Node* source, *copy;
+	Node* source, * copy;
 	source = k.head;
 	copy = head;
 	for (int i = 1; i < k._size; i++)
 	{
 		source = source->next;
-		copy->next = new Node(source->value,NULL,copy);
+		copy->next = new Node(source->value, NULL, copy);
 		copy = copy->next;
 	}
 	tail = copy;
-	
-	
+
+
 }
 template <typename T>
 void List<T>::print(Node* head)
 {
 	if (head == NULL) return;
-	Node* temp = head;  
-                   
-	do              
+	Node* temp = head;
+
+	do
 	{
-		cout << temp->data << " ";       
-		temp = temp->next;             
+		cout << temp->data << " ";
+		temp = temp->next;
 	} while (temp != head);
 	cout << "\n";
 	/*while (head != NULL)
@@ -102,7 +102,7 @@ void List<T>::print(Node* head)
 template <typename T>
 void List<T>::push_back(T val)
 {
-	if (head != NULL)                    
+	if (head != NULL)
 	{
 		Node* temp = new Node();
 		temp->data = val;
@@ -110,11 +110,11 @@ void List<T>::push_back(T val)
 		temp->prev = head->prev;
 		head->prev = temp;
 		temp->prev->next = temp;
-		
+
 	}
 	else //Если список пустой
 	{
-		               
+
 		head = new Node();
 		head->data = val;
 		head->prev = head;
@@ -122,25 +122,25 @@ void List<T>::push_back(T val)
 	}
 	_size++;
 }
-	/*
-	Node* temp = new  Node;
+/*
+Node* temp = new  Node;
 
-	
-	if (head == NULL)
-	{
-		head = new Node(val);
-		return;
-	}
-	Node* temp = head;
-	while (temp->next != NULL)
-	{
 
-		temp = temp->next;
-	}
-		
-	temp->next = new Node;
-	temp->next->value = val;
-	temp->next->next = NULL;
+if (head == NULL)
+{
+	head = new Node(val);
+	return;
+}
+Node* temp = head;
+while (temp->next != NULL)
+{
+
+	temp = temp->next;
+}
+
+temp->next = new Node;
+temp->next->value = val;
+temp->next->next = NULL;
 }*/
 template <typename T>
 void List<T>::push_top(T val)
@@ -175,10 +175,11 @@ void List<T>::pop()
 	}
 	else
 	{
-		
+
+		cout << "Error: Out of range";
+		return;
 	}
 }
-
 template <typename T>
 T List<T>::top()
 {
@@ -199,28 +200,11 @@ int List<T>::size()
 template <typename T>
 bool List<T>::empty()
 {
-	return _size == 0 ;
+	return _size == 0;
 }
-template <typename T>
-void List<T>::pop_front()
-{
 
-	if (head == NULL)
-	{
-		cout << "Error: Out of range";
-		return;
-	}
-
-	
-	Node* temp = head; //сохраняем последний элемент во временную переменную
-	if (head == tail) tail = NULL;
-	head = head->next; //предпоследний(тот,на который указывает последний) элемент делаем последним
-	
-	delete temp; //удаляем последний элемент
-	_size--;
-}
 template <typename T>
-T List<T>::get( int  pos)
+T List<T>::get(int  pos)
 {
 
 	if (pos == 0)
@@ -246,11 +230,11 @@ T List<T>::get( int  pos)
 		temp = tail;
 		for (int i = _size - 1; i > pos; i--)
 		{
-			temp= temp->prev;
+			temp = temp->prev;
 		}
 	}
 	return temp->value;
-	
+
 }
 template <typename T>
 void List<T>::erase(int  pos)
@@ -288,50 +272,50 @@ void List<T>::erase(int  pos)
 		prev->next = next;
 	if (next != NULL)
 		next->prev = prev;
-	
+
 	delete temp;
 	_size--;
 
 }
 template <typename T>
- List<T>::~List()
+List<T>::~List()
 {
-	 while (head != NULL)
-	 {
-		 Node* temp = head;
-		 head = head->next;
-		 delete temp;
+	while (head != NULL)
+	{
+		Node* temp = head;
+		head = head->next;
+		delete temp;
 	}
 }
- int main()
- {
-	 List <int> polka;
-	 int n;
-	 cin >> n;
-	 for (int i = 0; i < n; i++)
-	 {
-		 int type, number;
-		 cin >> type;
-		 switch (type)
-		 {
-		 case 1:
-			 cin >> number;
-			 polka.push_top(number);
-			 break;
-		 case 2:
-			 cin >> number;
-			 polka.push_back(number);
-			 break;
-		 case 3:
-			 cout << polka.top() << " ";
-			 polka.pop_front();
-			 break;
-		 case 4:
-			 cout << polka.get(polka.size() - 1) << " ";
-			 polka.pop();
-			 break;
-		 }
+int main()
+{
+	List <int> polka;
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		int type, number;
+		cin >> type;
+		switch (type)
+		{
+		case 1:
+			cin >> number;
+			polka.push_top(number);
+			break;
+		case 2:
+			cin >> number;
+			polka.push_back(number);
+			break;
+		case 3:
+			cout << polka.top() << " ";
+			polka.pop_front();
+			break;
+		case 4:
+			cout << polka.get(polka.size() - 1) << " ";
+			polka.pop();
+			break;
+		}
 
-	 }
+	}
 
- }
+}
