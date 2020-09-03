b2958cf9e4763709f03b381c9f22997c46ae4ba9
Елизавета Куличенко <Elicoolich@yandex.ru>
2020-09-03 12:34:57 +0300

    ///

--- Project11/Source.cpp
+++ Project11/Source.cpp
@@ -1,10 +1,12 @@
 #include<iostream>
 using namespace std;
-int rec2(int n)
+void rec2(int n)
 {
 	if (n==0)
-		return 0;
-	return n % 10 + rec2(n / 10);
+		return ;
+	rec2(n / 10);
+	cout<< n % 10<<" ";
+		
 	//sum = n % 10 + (n / 10) % 10 + (n / 10 / 10) % 10 + ... + 0;
 }
 int main()
@@ -12,5 +14,5 @@ int main()
 	int n;
 	cin >> n;
 	//rec(n);
-	cout<<rec2(n);
+	rec2(n);
 }
