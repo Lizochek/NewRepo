a465fbc854af807ddec7c68322859d74705db047
Елизавета Куличенко <Elicoolich@yandex.ru>
2020-09-09 15:56:16 +0300

    U: Разворот числа Дано число n, десятичная запись которого не содержит нулей. Получите число, записанное теми же цифрами, но в противоположном порядке. При решении этой задачи нельзя использовать циклы, строки, списки, массивы, разрешается только рекурсия и целочисленная арифметика. Фунция должна возвращать целое число, являющееся результатом работы программы, выводить число по одной цифре нельзя.

--- Project11/Source.cpp
+++ Project11/Source.cpp
@@ -1,27 +1,16 @@
 #include<iostream>
 using namespace std;
-void rec2(string s)
+int rec2(int n)
 {
-	cout << s << endl;
-	if (s.size() <= 1 )
-	{
-		cout << "yes";
-		return;
-	}
-	if (s[0] != s[s.size() - 1])
-	{
-		cout << "no";
-		return;
-	}
-	rec2(s.substr(1, s.size()-2));
+	static int r = 0;
+	if (n==0) return r;
+	r = r*10+(n % 10);
+	return rec2(n/10);
 	
-		
-	//sum = n % 10 + (n / 10) % 10 + (n / 10 / 10) % 10 + ... + 0;
 }
 int main()
 {
-	string s;
-	cin >> s;
-	//rec(n);
-	rec2(s);
+	int n;
+	cin >> n;
+	cout << rec2(n);
 }
