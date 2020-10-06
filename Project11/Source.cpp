f605c6a0470eb624c0a503040fcac684037551a3
Елизавета Куличенко <Elicoolich@yandex.ru>
2020-10-06 13:01:13 +0300

    реализация с инкапсуляцией работы  с узлами в базовом классе

--- Project11/Source.cpp
+++ Project11/Source.cpp
@@ -1,9 +1,8 @@
 #include<iostream>
 using namespace std;
-template <typename T>
-class List {
 
-public: struct Node {
+template <typename T>
+struct Node {
 	Node() { this->next = NULL; this->prev = NULL; };
 	Node(T value, Node* next, Node* prev)
 	{
@@ -13,20 +12,150 @@ public: struct Node {
 	}
 	Node(T value)
 	{
-		Node(value, NULL, NULL);
+		this->value = value;
+		this->next = NULL;
+		this->prev = NULL;
 
 	}
 	T value;
 	Node* next;
 	Node* prev;
 };
-private: Node* head;
+
+template <typename T>
+class BaseCycleList {
+public:
+	/*Создаёт новый список,содержащий нод*/
+	static Node<T>* create(Node<T>* node);
+	/*Метод, добавляет новый узел со значением val, после узла nodeOfClist
+	в  двунаправленном циклическом списке
+	Возвращает указатель на вновь созданный узел
+	*/
+	static Node<T>* add(Node<T>* nodeOfClist, T value);
+	/*Метод, добавляет узел node, после узла nodeOfClist
+	в  двунаправленном циклическом списке
+	Возвращает указатель на вновь добавленный узел
+	*/
+	static Node<T>* add(Node<T>* nodeOfClist, Node<T>* node);
+	/*Метод, удаляет узел node из
+	в  двунаправленного циклического списка
+	Возвращает указатель на удалённый узел*/
+	static Node<T>* erase(Node<T>* nodeOfClist);
+	/*Метод, ищет узел со значением value в
+	в  двунаправленном циклическом списке, начиная с узлаn odeOfClist
+	Возвращает указатель найденный узел или NULL*/
+	static Node<T>* find(Node<T>* nodeOfClist, T value);
+	/* Возвращает значение для узла*/
+	static T  get(Node<T>*);
+	/* Устанавливает значение для узла
+   Возвращает ссылку на узел*/
+	static Node<T>* set(Node<T>*, T);
+	/*Возврщает ссылку на следующий за nodeOfClist узел*/
+	static  Node<T>* next(Node<T>* nodeOfClist);
+	/*Возврщает ссылку на предыдущий за nodeOfClist узел*/
+	static  Node<T>* prev(Node<T>* nodeOfClist);
+
+};
+/*Создаёт новый список,содержащий нод*/
+template <typename T>
+Node<T>* BaseCycleList<T>::create( Node<T>* node)
+{
+	node->next=node ;
+	node->prev = node;
+	return node;
+}
+/*Метод, добавляет новый узел со значением val, после узла nodeOfClist
+в  двунаправленном циклическом списке
+Возвращает указатель на вновь созданный узел*/
+template <typename T>
+Node<T>* BaseCycleList<T>::add(Node<T>* nodeOfClist, T val)
+{
+	Node<T>* node = new Node<T>(val);
+	return BaseCycleList::add(nodeOfClist, node);
+}
+/*Метод, добавляет узел node, после узла nodeOfClist
+в  двунаправленном циклическом списке
+Возвращает указатель на вновь добавленный узел*/
+template <typename T>
+Node<T>* BaseCycleList<T>::add(Node<T>* nodeOfClist, Node<T>* node)
+{
+	Node<T>* next = nodeOfClist->next;
+
+	node->prev = nodeOfClist;
+	node->next = next;
+
+	next->prev = node;
+	nodeOfClist->next = node;
+	return node;
+}
+/*Метод, удаляет узел node из
+в  двунаправленного циклического списка
+Возвращает указатель на удалённый узел*/
+template <typename T>
+Node<T>* BaseCycleList<T>::erase(Node<T>* node)
+{
+	Node<T>* next = node->next;
+	Node<T>* prev = node->prev;
+
+	next->prev = prev;
+	prev->next = next;
+
+	return node;
+}
+/*Метод, ищет узел со значением value в
+в  двунаправленном циклическом списке, начиная с узлаn odeOfClist
+Возвращает указатель найденный узел или NULL*/
+template <typename T>
+Node<T>* BaseCycleList<T>::find(Node<T>* nodeOfClist, T value)
+{
+	Node<T>* it = nodeOfClist;
+	do
+	{
+		if (it->value == value)
+			return it;
+		it = it->next;
+	} while (it != nodeOfClist);
+	return NULL;
+}
+/* Возвращает значение для узла*/
+template <typename T>
+T BaseCycleList<T>::get(Node<T>* node)
+{
+	return node->value;
+}
+/* Устанавливает значение для узла
+Возвращает ссылку на узел*/
+template <typename T>
+Node<T>* BaseCycleList<T>::set(Node<T>* node, T value)
+{
+	node->value = value;
+	return node;
+}
+/*Возврщает ссылку на следующий за nodeOfClist узел*/
+template <typename T>
+ Node<T>* BaseCycleList<T>::next(Node<T>* nodeOfClist)
+{
+	return nodeOfClist->next;
+}
+/*Возврщает ссылку на предыдущий за nodeOfClist узел*/
+template <typename T>
+ Node<T>* BaseCycleList<T>::prev(Node<T>* nodeOfClist)
+{
+	return nodeOfClist->prev;
+}
+
+
+template <typename T>
+class Deque {
+
+
+private: Node<T>* head;
 	   int _size;
 
 public:
-	List();
-	List(const List&);
-	void print(Node* head);
+	Deque();
+	Deque(const Deque&);
+	void print(Node<T>* head);
 	void insert(int pos, T val);
 	void push_top(T val);
 	void push_back(T val);
@@ -37,23 +166,23 @@ public:
 	void erase(int pos);
 	int size();
 	bool empty();
-	~List();
+	~Deque();
 
 
 };
 template <typename T>
-List<T>::List()
+Deque<T>::Deque()
 {
 	_size = 0;
-	head = NULL;
+	head = BaseCycleList<T>::create(new Node<T>());
 }
 template <typename T>
-List<T>::List(const List& k)
+Deque<T>::Deque(const Deque& k) :Deque()
 {
 	/*
 	когда вызвается:
-	List <int> a;
-	List<int> b;
+	Deque <int> a;
+	Deque<int> b;
 	a = b;
 	передаем в фун-цию по значению
 	function(a);
@@ -61,152 +190,112 @@ List<T>::List(const List& k)
 	return a;
 	*/
 	_size = k._size;
+	BaseCycleList::set(head, k.head->value);
 
-	if (k.head == NULL) return;
-
-	head = new Node(k.head->value);
 	Node* source, * copy;
 	source = k.head;
 	copy = head;
 	for (int i = 1; i < k._size; i++)
 	{
 		source = source->next;
-		copy->next = new Node(source->value, NULL, copy);
-		copy = copy->next;
+		copy = BaseCycleList::add(copy, source->value);
 	}
-	copy->next = head;
-	head->prev = copy;
 }
 template <typename T>
-void List<T>::print(Node* head)
+void Deque<T>::print(Node<T>* head)
 {
-	if (head == NULL) return;
+	if (empty()) return;
 	Node* temp = head;
 
 	do
 	{
-		cout << temp->data << " ";
-		temp = temp->next;
+		cout << BaseCycleList::get(temp) << " ";
+		temp = BaseCycleList::next(temp);
 	} while (temp != head);
 	cout << "\n";
 }
 template <typename T>
-void List<T>::insert(int pos,T val)
+void Deque<T>::insert(int pos, T val)
 {
 	if (pos > _size) {
-		cout<<"Error: Out of range";
+		cout << "Error: Out of range";
 		return;
 	}
 	if (pos == 0) push_top(val);
 	else if (pos == _size) push_back(val);
-	else 
+	else
 	{
 		Node* temp = head;
-		for (int i = 0; i < pos-1; ++i)
-			temp = temp->next;
-		Node* node = new Node(val, temp->next, temp);
-		temp->next = node;
-		node->next->prev = node;
+		//todo: можно оптимальней, если бежать не только с начала, 
+		//но и с конца, когда так ближе
+		for (int i = 0; i < pos - 1; ++i)
+			temp = BaseCycleList::next(temp);
+		BaseCycleList::add(temp, val);
 		_size++;
 	}
 
 }
 template <typename T>
-void List<T>::push_back(T val)
+void Deque<T>::push_back(T val)
 {
-	if (head != NULL)
-	{
-		Node* temp = new Node();
-		temp->value = val;
-		temp->next = head;
-		temp->prev = head->prev;
-		head->prev = temp;
-		temp->prev->next = temp;
-
-	}
-	else //Если список пустой
-	{
-
-		head = new Node();
-		head->value = val;
-		head->prev = head;
-		head->next = head;
-	}
+	if (_size == 0) BaseCycleList<T>::set(head, val);
+	else BaseCycleList<T>::add(BaseCycleList<T>::prev(head), val);
 	_size++;
 }
 template <typename T>
-void List<T>::push_top(T val)
+void Deque<T>::push_top(T val)
 {
 	push_back(val);
-	head = head->prev;
+	head=BaseCycleList<T>::prev(head);
 }
 template <typename T>
-void List<T>::pop()
+void Deque<T>::pop()
 {
-	if (head != NULL)
+	if (_size!=0 )
 	{
-		Node* temp = head->prev;
-		head->prev = temp->prev;
-		temp->prev->next = head;
-		delete temp;
+		if (_size > 1) 
+			delete BaseCycleList<T>::erase(BaseCycleList<T>::prev(head));
 		_size--;
-		if (_size == 0)
-			head = NULL;
 	}
-	
-	else
-	{
-		cout << "Error: Out of range";
-		return;
-	}
-}
-template <typename T>
-void List<T>::pop_front()
-{
-	if (_size > 0)
-	{
-		Node* temp = head;
-		head = head->next;
-		head->prev = temp->prev;
-		temp->prev->next = head;
-		delete temp;
-		_size--;
-		if (_size == 0)
-			head = NULL;
-	}
-	else
-	{
 
+	else
+	{
 		cout << "Error: Out of range";
 		return;
 	}
 }
 template <typename T>
-T List<T>::top()
+void Deque<T>::pop_front()
 {
-	if (head == NULL)
+	head = BaseCycleList<T>::next(head);
+	pop();
+}
+template <typename T>
+T Deque<T>::top()
+{
+	if (empty())
 	{
-		cout << "Error: List is empty!";
+		cout << "Error: Deque is empty!";
 		return NULL;
 	}
 
-	return head->value;
+	return BaseCycleList<T>::get(head);
 
 }
 
 template <typename T>
-int List<T>::size()
+int Deque<T>::size()
 {
 	return _size;
 }
 template <typename T>
-bool List<T>::empty()
+bool Deque<T>::empty()
 {
 	return _size == 0;
 }
 
 template <typename T>
-T List<T>::get(int  pos)
+T Deque<T>::get(int  pos)
 {
 
 	if (pos == 0)
@@ -218,13 +307,13 @@ T List<T>::get(int  pos)
 		cout << "Error";
 		return NULL;
 	}
-	Node* temp;
+	Node<T>* temp;
 	if (pos < _size / 2) {
 		temp = head;
 
 		for (int i = 0; i < pos; i++)
 		{
-			temp = temp->next;
+			temp=BaseCycleList<T>::next(temp);
 		}
 	}
 	else
@@ -232,14 +321,14 @@ T List<T>::get(int  pos)
 		temp = head;
 		for (int i = _size; i > pos; i--)
 		{
-			temp = temp->prev;
+			temp = BaseCycleList<T>::prev(temp);
 		}
 	}
-	return temp->value;
+	return BaseCycleList<T>::get(temp);
 
 }
 template <typename T>
-void List<T>::erase(int  pos)
+void Deque<T>::erase(int  pos)
 {
 	if (pos == 0)
 	{
@@ -257,7 +346,7 @@ void List<T>::erase(int  pos)
 
 		for (int i = 0; i < pos; i++)
 		{
-			temp = temp->next;
+			temp = BaseCycleList::next(temp);
 		}
 	}
 	else
@@ -265,32 +354,27 @@ void List<T>::erase(int  pos)
 		temp = head;
 		for (int i = _size; i > pos; i--)
 		{
-			temp = temp->prev;
+			temp = BaseCycleList<T>::prev(temp);
 		}
 	}
-	Node* prev = temp->prev;
-	Node* next = temp->next;
-		prev->next = next;
-		next->prev = prev;
-
-	delete temp;
+	delete BaseCycleList<T>::erase(temp);
 	_size--;
 
 }
 template <typename T>
-List<T>::~List()
+Deque<T>::~Deque()
 {
-	while (_size>0)
+	while (_size > 0)
 	{
-		Node* temp = head;
-		head = head->next;
+		Node<T>* temp = head;
+		head = BaseCycleList<T>::next(head);
 		delete temp;
 		_size--;
 	}
 }
 int main()
 {
-	List <int> polka;
+	Deque <int> polka;
 	int n;
 	cin >> n;
 	for (int i = 0; i < n; i++)
